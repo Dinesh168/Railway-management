972ae313c5fd637bd2d6c0ab6180fa73f7f210ce
Dinesh R <117595292+Dinesh168@users.noreply.github.com>
2023-03-28 11:23:35 +0530

    Add files via upload

---
+++ Railway/src/main/java/com/mycompany/railway/Railway.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.railway;
+
+/**
+ *
+ * @author Dinesh R
+ */
+public class Railway {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
